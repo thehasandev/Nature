43582534866932d3fd3ced0902b7228729e0b09b
Hasan <thehasandev@gmail.com>
2023-08-18 18:54:24 +0600

    add

---
+++ js/script.js
@@ -0,0 +1,22 @@
+tailwind.config = {
+  theme: {
+    extend: {
+      colors: {
+        clifford: "#da373d",
+        ton: "#FFC178",
+        tin: "#FFA343",
+        kan: "rgb(0 0 0 5)",
+      },
+      maxWidth: {
+        container: "1320px",
+      },
+
+      fontFamily: {
+        san: ["Open Sans", "sans-serif"],
+      },
+      backgroundImage: {
+        hero: "url('./images/banner.png')",
+      },
+    },
+  },
+};
